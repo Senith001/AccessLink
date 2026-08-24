e1448aa709f403782a696d01ca8af9440450b915
Senith001 <senithpasindu@gmail.com>
2026-08-24 20:27:00 +0530

    Implement login and registration authentication

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // Login
   Future<UserCredential> loginUser(
     String email,
     String password,
@@ -13,6 +14,18 @@ class AuthService {
     );
   }
 
+  // Registration
+  Future<UserCredential> registerUser(
+    String email,
+    String password,
+  ) async {
+    return await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  // Logout
   Future<void> logoutUser() async {
     await _auth.signOut();
   }
